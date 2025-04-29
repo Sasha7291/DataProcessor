81db7e97c4c57fe70e10662dc048aeec25e98654
Terentev A.D. <terentev.ad18@physics.msu.ru>
2025-04-30 01:20:56 +0300

    Fix Integration

--- include/psr_integration.h
+++ include/psr_integration.h
@@ -2,6 +2,9 @@
 
 #include "psr_common.h"
 
+#include <QDebug>
+
+
 namespace psr
 {
 
@@ -21,6 +24,7 @@ template<class T>
 double Integration<T>::operator()(ConstInputRange<T> x, ConstInputRange<T> y) const noexcept
 {
     double result = 0.0;
+    qDebug() << x.size() << y.size();
 
     for (auto i = 1; i < x.size() - 1; ++i)
         result += 0.5 * (y[i] + y[i + 1]) * (x[i + 1] - x[i]);
