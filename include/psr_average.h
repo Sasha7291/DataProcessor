69e102dabae03bccd4b677fabe9458fde8b13168
Terentev A.D. <terentev.ad18@physics.msu.ru>
2025-05-25 01:33:07 +0300

    Fix CMakeLists.txt and Average

--- include/psr_average.h
+++ include/psr_average.h
@@ -11,33 +11,33 @@ class Average
 {
 
 public:
-    Average() noexcept = default;
-    ~Average() noexcept = default;
+    Average() = default;
+    ~Average() = default;
 
-    [[nodiscard(R"(Time omplexity O(data.size()))")]] double operator()(ConstInputRange<T> data) const noexcept;
-    [[nodiscard(R"(Time complexity O(data.size()))")]] double operator()(ConstInputRange<T> weights, ConstInputRange<T> data) const noexcept;
-    [[nodiscard(R"(Time complexity O(data.size()))")]] double operator()(ConstInputRange<T> data, unsigned n) const noexcept;
+    [[nodiscard(R"(Time omplexity O(data.size()))")]] double operator()(ConstInputRange<T> data) const;
+    [[nodiscard(R"(Time complexity O(data.size()))")]] double operator()(ConstInputRange<T> weights, ConstInputRange<T> data) const;
+    [[nodiscard(R"(Time complexity O(data.size()))")]] double operator()(ConstInputRange<T> data, int n) const;
 
 };
 
 template<class T>
-double Average<T>::operator()(ConstInputRange<T> data) const noexcept
+double Average<T>::operator()(ConstInputRange<T> data) const
 {
     return std::accumulate(data.cbegin(), data.cend(), 0.0) / data.size();
 }
 
 template<class T>
-double Average<T>::operator()(ConstInputRange<T> weights, ConstInputRange<T> data) const noexcept
+double Average<T>::operator()(ConstInputRange<T> weights, ConstInputRange<T> data) const
 {
     return std::transform_reduce(data.cbegin(), data.cend(), weights.cbegin(), 0.0, std::plus<>(), std::multiplies<>())
         / std::accumulate(weights.cbegin(), weights.cend(), 0.0);
 }
 
 template<class T>
-double Average<T>::operator()(ConstInputRange<T> data, unsigned n) const noexcept
+double Average<T>::operator()(ConstInputRange<T> data, int n) const
 {
     return std::pow(
-        std::transform_reduce(data.cbegin(), data.cend(), 0.0, std::plus<>(), std::bind(std::pow<double, int>, std::placeholders::_1, n)) / data.size(),
+        std::transform_reduce(data.cbegin(), data.cend(), 0.0, std::plus<>(), std::bind(static_cast<double(*)(double, int)>(std::pow), std::placeholders::_1, n)) / data.size(),
         1.0 / n
     );
 }
