8792034b8b125d70484b34b4e2f8e68ee88abbb8
Александр Д. Терентьев <Terentev@nanoscopy.org>
2025-12-29 11:56:24 +0300

    Add Unique

--- include/psr_fouriertransform.h
+++ include/psr_fouriertransform.h
@@ -22,19 +22,19 @@ private:
 };
 
 template<class T>
-std::pair<OutputRange<T>, OutputRange<T>> FourierTransform<T>::operator ()(ConstInputRange<T> data, double period) const
+std::pair<OutputRange<T>, OutputRange<T>> FourierTransform<T>::operator()(ConstInputRange<T> data, double period) const
 {
     OutputRange<T> resultValues;
     OutputRange<T> resultKeys;
     resultValues.reserve(data.size());
     resultKeys.reserve(data.size());
 
-    for (std::size_t k = 0; k < data.size(); ++k)
+    for (decltype(data.size()) k = 0; k < data.size(); ++k)
     {
         std::complex<T> temp{0.0, 0.0};
 
-        for (std::size_t n = 0; n < data.size(); ++n)
-            temp += data[n] * std::exp(-2 * std::numbers::pi * 1i * k * n / data.size());
+        for (decltype(data.size()) n = 0; n < data.size(); ++n)
+            temp += data[n] * std::exp(-2 * std::numbers::pi * std::complex<T>(0, 1) * k * n / data.size());
 
         resultValues.push_back(std::abs(temp));
         resultKeys.push_back(k / (data.size() * period));
---
+++ include/psr_unique.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "psr_common.h"
+
+
+namespace psr
+{
+
+template<class T>
+class Unique
+{
+
+public:
+    Unique() = default;
+    ~Unique() = default;
+
+    [[nodiscard(R"(Time complexity O(data.size()))")]] OutputRange<T> operator()(ConstInputRange<T> data) const;
+
+};
+
+
+template<class T>
+OutputRange<T> Unique<T>::operator()(ConstInputRange<T> data) const
+{
+    OutputRange<T> result(data.cbegin(), data.cend());
+
+    std::ranges::sort(result);
+    std::ranges::unique(result);
+
+    return result;
+}
+
+}
